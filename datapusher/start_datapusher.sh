64c08c7108c2c7593b26b7deba77a3f1ce531457
Giuseppe Ranieri Musella <ranierimusella2@msn.com>
2022-02-07 12:19:14 +0100

    DaPu STDERR logs settings

---
+++ datapusher/start_datapusher.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#            --module wsgi:application \
+UWSGI_OPTS="--plugins http,python \
+            --socket /tmp/uwsgi.sock \
+            --wsgi-file ${APP_DIR}/datapusher.wsgi \
+            --uid www-data --gid www-data \
+            --http 0.0.0.0:8800 \
+            --master --enable-threads \
+            --lazy-apps \
+            -p 4 -L -b 32768 --vacuum \
+            --harakiri $UWSGI_HARAKIRI"
+uwsgi $UWSGI_OPTS
